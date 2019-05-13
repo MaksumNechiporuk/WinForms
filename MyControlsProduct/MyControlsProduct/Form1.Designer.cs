073a92c98da075be29b590f5075502a07bd95b87
MaksumNechiporuk <maksum.nec@gmail.com>
2019-05-13 12:00:53 +0300

    My project

---
+++ MyControlsProduct/MyControlsProduct/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MyControlsProduct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myCotrols1 = new MyControlsProduct.MyCotrols();
+            this.SuspendLayout();
+            // 
+            // myCotrols1
+            // 
+            this.myCotrols1.Location = new System.Drawing.Point(13, 13);
+            this.myCotrols1.Name = "myCotrols1";
+            this.myCotrols1.Size = new System.Drawing.Size(507, 287);
+            this.myCotrols1.TabIndex = 0;
+            this.myCotrols1.Load += new System.EventHandler(this.myCotrols1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 312);
+            this.Controls.Add(this.myCotrols1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyCotrols myCotrols1;
+    }
+}
+
---
+++ MyControlsProduct/MyControlsProduct/MyCotrols.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyControlsProduct
+{
+    public partial class MyCotrols : UserControl
+    {
+        List<Category> categories = null;
+        List<Product> products =null;
+        public MyCotrols()
+        {
+            InitializeComponent();
+        }
+
+        private void MyCotrols_Load(object sender, EventArgs e)
+        {
+            categories = new List<Category>()
+            {
+                new Category{Id=1,Name="Смартфони"},
+                new Category{Id=2,Name="Товари для дому"},
+                new Category{Id=3,Name="Спорт"},
+                new Category{Id=4,Name="Канцтовари"}
+            };
+            comboBox1.DataSource = categories;
+            comboBox1.ValueMember = "Id";
+            comboBox1.DisplayMember = "Name";
+
+
+            products = new List<Product>()
+            {
+                new Product{Id=1,Name="Samsung", CategoryId = 1, Category = categories.Where(x=>x.Id==1).FirstOrDefault()},
+                new Product{Id=2,Name="Шафа", CategoryId = 2, Category = categories.Where(x=>x.Id==2).FirstOrDefault()},
+                new Product{Id=3,Name="Xiaomi", CategoryId = 1, Category = categories.Where(x=>x.Id==1).FirstOrDefault()},
+                new Product{Id=4,Name="Бігова доріжка", CategoryId = 3, Category = categories.Where(x=>x.Id==3).FirstOrDefault()}
+            };
+       
+
+            comboBox2.DataSource = products.Where(x => x.Category == (Category)comboBox1.SelectedItem).ToList();
+            comboBox2.ValueMember = "Id";
+            comboBox2.DisplayMember = "Name";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.DataSource = null;
+
+            if (products != null)
+            {
+                comboBox2.DataSource = products.Where(x => x.Category == (Category)comboBox1.SelectedItem).ToList();
+                comboBox2.ValueMember = "Id";
+                comboBox2.DisplayMember = "Name";
+            }
+        }
+    }
+  public  class Category
+    {
+       public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
