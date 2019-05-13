6af00de68e1ab033f188427e279a23a291ea02c4
MaksumNechiporuk <maksum.nec@gmail.com>
2019-05-13 11:05:29 +0300

    add my control

---
+++ MYControls/MYControls/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MYControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.valid1 = new MYControls.Valid();
+            this.SuspendLayout();
+            // 
+            // valid1
+            // 
+            this.valid1.Location = new System.Drawing.Point(69, 45);
+            this.valid1.Name = "valid1";
+            this.valid1.Size = new System.Drawing.Size(173, 124);
+            this.valid1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 234);
+            this.Controls.Add(this.valid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Valid valid1;
+    }
+}
+
---
+++ MYControls/MYControls/Valid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MYControls
+{
+    public partial class Valid : UserControl
+    {
+        public Valid()
+        {
+            InitializeComponent();
+            FValid();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            FValid();
+        }
+        void FValid()
+        {
+            if (textBox1.Text == "")
+                label1.Text = "Empty";
+            else
+                label1.Text = "";
+        }
+    }
+}
